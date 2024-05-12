b94a81c5f6a662b2d379200027a619bd600d8cb6
NguyenNHuHoai <nguyennhuhoai745@gmail.com>
2024-05-12 17:24:38 +0700

    code section 3

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import DesignConfigurator from "@/app/configure/design/DesignConfigurator";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import React from "react";
 
-const Page = async () => {
-    
-  return (
-    <div>
-      
-    </div>
-  )
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
 
-export default Page
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDemensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
+};
+
+export default Page;
