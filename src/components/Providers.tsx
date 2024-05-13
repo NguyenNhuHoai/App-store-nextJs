1e098691ebc4b5678e24bcfdb3e097b1dbda0350
NguyenNHuHoai <nguyennhuhoai745@gmail.com>
2024-05-13 15:44:16 +0700

    update to day

---
+++ src/components/Providers.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React, { ReactNode } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const client = new QueryClient();
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+};
+
+export default Providers;
